f762fa3d8f3b209f2e4897a9c758e4b4b400320c
Saddam Hussain <saddam.hussain112030@gamil.com>
2025-07-05 01:54:22 +0530

    almost done

--- BackEnd/controllers/orderController.js
+++ BackEnd/controllers/orderController.js
@@ -103,4 +103,26 @@ const userOrders=async (req,res)=>{
         res.json({success:false, message:"Error"})
       }
 }
-export { placeOrder ,verifyOrder,userOrders};
+
+// List of ordered items for admin
+const listOrders=async(req,res)=>{
+      try {
+          const orders=await orderModel.find({});
+          res.json({success:true,data:orders});
+      } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+      }
+}
+// api for order status update by admin
+
+const updateStatus=async(req,res)=>{
+      try {
+          await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status})
+          res.json({success:true,message:"Status Updated"})
+      } catch (error) {
+          console.log(error);
+          res.json({success:false,message:"Error"})
+      }
+}
+export { placeOrder ,verifyOrder,userOrders,listOrders,updateStatus};
